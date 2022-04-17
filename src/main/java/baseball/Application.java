1d06d3c55f584e9dfd751cc039314f16ba0da536
hsn <hsnam@tdgl.co.kr>
2022-04-17 12:34:25 +0900

    feat : main 연결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+import baseball.model.BallGenerator;
+import baseball.model.BallMatcher;
+import baseball.model.BallValidator;
+import baseball.view.BaseballView;
+
+import static baseball.constant.Constant.*;
+import static baseball.constant.Constant.BALL_COUNT;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        BaseballController controller = new BaseballController.BaseballControllerBuilder()
+                .matcher(BallMatcher.of(BALL_COUNT))
+                .validator(BallValidator.of(BALL_COUNT))
+                .generator(BallGenerator.of(BALL_COUNT, NUMBER_MIN, NUMBER_MAX))
+                .view(BaseballView.of(BALL_COUNT))
+                .build();
+
+        controller.run();
     }
 }
